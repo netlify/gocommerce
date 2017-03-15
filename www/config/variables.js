5d2fe1f489cc0f1b5eef032d4fdee073eec4789b
Eli Williamson <eliwilliamson@gmail.com>
2017-03-15 14:12:31 -0400

    add www page

---
+++ www/config/variables.js
@@ -0,0 +1,46 @@
+// if you change these you must restart the server
+
+module.exports = {
+
+  // colors
+  lightestGrey: '#E6E6E6',
+  lighterGrey: '#F7F8F8',
+  lightGrey: '#F6F6F6',
+  grey: '#313D3E',
+  darkGrey: '#2F3132',
+  darkerGrey: '#1C1E1E',
+  lightGreen: '#5a874a',
+  green: '#76b852',
+  darkGreen: '#8DC26F',
+  orange: '#e8a751',
+
+  // typography
+  thin: 100,
+  light: 300,
+  regular: 400,
+  semibold: 500,
+  bold: 700,
+  black: 900,
+
+  // fonts
+  roboto: "'Roboto', -apple-system, BlinkMacSystemFont, Helvetica, Arial, sans-serif",
+
+  // padding
+  micro: '8px',
+  tiny: '16px',
+  small: '24px',
+  medium: '40px',
+  large: '64px',
+  xl: '104px',
+  xxl: '168px',
+
+  // border radius
+  borderRadius: '4px',
+  largeBorderRadius: '10px',
+
+  // responsive breakpoints
+  mobile: '480px',
+  tablet: '768px',
+  desktop: '960px',
+  display: '1200px'
+}
